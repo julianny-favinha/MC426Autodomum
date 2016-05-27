a75fd059778f6593582fe4285ac8341a22b8c9bb
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2016-05-27 17:59:11 -0300

    add logout

--- Perfis/server/core/src/main/resources/static/index.js
+++ Perfis/server/core/src/main/resources/static/index.js
@@ -1,12 +1,27 @@
 'use strict';
 
 angular.module('autodomun.index', ['ngRoute'])
-.controller('IndexController', function($scope, $location) {
+.controller('IndexController', function($scope, $http, $location) {
     $scope.isLoginPage = function() {
         return $location.path() == '/';
     }
 
     $scope.home = function() {
         $location.path('/home');
+    }
+
+    $scope.logout = function() {
+        $http({
+            method: 'GET',
+            url: '/api/logout',
+        }).then(function successCallback(response) {
+            if(response.status != 200) {
+                $scope.error = true;
+            } else {
+                $location.path('/');
+            }
+        }, function errorCallback(response) {
+            $scope.error = true;
+        });
     }
 });
