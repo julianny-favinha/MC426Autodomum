e2fc6d68338d170b7ff7bef2f346ab2782e21ff5
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2016-06-19 15:11:30 -0300

    adicionou flag de admin na edição de usuário

--- Perfis/server/core/src/main/resources/static/features/profiles/profile.js
+++ Perfis/server/core/src/main/resources/static/features/profiles/profile.js
@@ -12,21 +12,43 @@ angular.module('autodomun.profile', ['ngRoute'])
 .controller('ProfileController', function($scope, $location, userService, permissionService, $anchorScroll) {
     $scope.user = {};
     $scope.user.permissoes = [];
+    $scope.admin = false;
 
     $scope.back = function() {
         $location.path("/profiles");
     }
 
-    $scope.permissionIndex = function(idPermissao) {
+    $scope.toggleAdmin = function() {
+        if($scope.admin) {
+            $scope.user.permissoes = $scope.permissoes.map(function(permission) {
+                return permission.id;
+            });
+        } else {
+            $scope.user.permissoes = [];
+            //TODO show dialog asking to add permissions
+        }
+    }
+
+    function permissionIndex(idPermissao) {
         return $scope.user.permissoes.indexOf(idPermissao);
     }
 
+    $scope.isPermissionSelected = function(idPermissao) {
+        return $scope.user.permissoes[permissionIndex(idPermissao)];
+    }
+
     $scope.togglePermissao = function(idPermissao) {
-        var index = $scope.permissionIndex(idPermissao);
+        var index = permissionIndex(idPermissao);
         if(index < 0) {
             $scope.user.permissoes.push(idPermissao);
         } else {
             $scope.user.permissoes.splice(index, 1);
+        }
+
+        if($scope.user.permissoes.length == $scope.permissoes.length) {
+            $scope.admin = true;
+        } else {
+            $scope.admin = false;
         }
     }
 
@@ -39,20 +61,19 @@ angular.module('autodomun.profile', ['ngRoute'])
             });
     }
 
-    $scope.admin = true; //TODO
-
     $anchorScroll();
-
-    userService.getByUsername($location.search()["username"])
-        .then(function successCallback(response) {
-            $scope.user = response.data;
-        }, function errorCallback(response) {
-            $scope.error = true;
-        });
 
     permissionService.getPermissions()
         .then(function successCallback(response) {
             $scope.permissoes = response.data;
+        }, function errorCallback(response) {
+            $scope.error = true;
+        });
+
+    userService.getByUsername($location.search()["username"])
+        .then(function successCallback(response) {
+            $scope.user = response.data;
+            $scope.admin = $scope.user.permissoes.length == 4; //FIXME
         }, function errorCallback(response) {
             $scope.error = true;
         });
